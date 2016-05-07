31f4187e370f85cdbfdd760a82f26fa5c65db5dc
Oskar Gewalli <gewalli@gmail.com>
2016-05-07 12:41:54 +0200

    NuGet package

--- FluentMigrator.Runner.ConnectionExecutor/Properties/AssemblyInfo.cs
+++ FluentMigrator.Runner.ConnectionExecutor/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FluentMigrator.Runner.ConnectionExecutor")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A Task executor for fluent migrator that accepts a connection.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("FluentMigrator.Runner.ConnectionExecutor")]
